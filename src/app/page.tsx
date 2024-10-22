2627b35ff4d230175bb7d0d88675fea1953807cd
hamail <mhamail1223@gmail.com>
2024-10-22 20:57:40 +0500

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import React, { useState } from "react";
 import SimpleTable from "@/components/table/SimpleTable";
 
 import { usePipeTableColumns } from "@/components/table/tableColumns/pipeTableColumns";
-import { pipeData, pipe_pn16_data } from "@/utils/pipdata";
+import { pipeData } from "@/utils/pipdata";
+import TextField from "@/components/hamailui/dataEntry/TextField";
 
 const Home = () => {
   const [percentage, setPercentage] = useState<number>(10);
-
+  const [data, setData] = useState(pipeData);
   const columns: any = usePipeTableColumns(percentage, setPercentage);
 
   const showTable = (item: { title: string; data: [] }) => {
@@ -22,7 +23,34 @@ const Home = () => {
       </div>
     );
   };
-  return pipeData.map((item: any) => showTable(item));
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchValue = e.target.value.toLowerCase(); // Convert to lowercase
+    const updateData = pipeData.filter(
+      (item) => item.title.toLowerCase().includes(searchValue) // Use includes for partial matches
+    );
+    setData(updateData);
+  };
+  return (
+    <div>
+      <div className="flex">
+        <TextField
+          placeholder="search"
+          className="rounded px-2 text-black"
+          onChange={handleSearch}
+        />
+        <TextField
+          placeholder="%"
+          type="number"
+          min={0}
+          max={100}
+          value={percentage}
+          className="rounded px-2 text-black ms-4"
+          onChange={(e) => setPercentage(Number(e.target.value))}
+        />
+      </div>
+      {data.map((item: any) => showTable(item))}
+    </div>
+  );
 };
 
 export default Home;
